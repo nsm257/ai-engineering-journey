322edfdd89b4b3ddad718f1fdac07f34d948912b
Narinder Singh Mehra <naren_mehra@yahoo.com>
2026-08-09 13:00:42 +0530

    Adding type hints

--- python-basics/day01-data.py
+++ python-basics/day01-data.py
@@ -23,7 +23,10 @@ def print_documents_id_and_title(documents):
 def print_total_num_documents(documents):
     print(f"Total number of documents: {len(documents)}")
 
-def get_document_by_id(documents, doc_id):
+def get_document_by_id(
+        documents: list[dict], 
+        doc_id: int
+) -> dict | None:
     for document in documents:
         if document['id'] == doc_id:
             return document
@@ -34,6 +37,22 @@ def print_word_count_of_each_document(documents):
         word_count = len(document['content'].split())
         print(f"Document ID: {document['id']} has {word_count} words.")
 
+def count_words(text:str) -> int:
+    return len(text.split())
+
+def summarize_documents(documents):
+    summaries = []
+    for document in documents:
+        summary = {
+            "id": document['id'],
+            "title": document['title'],
+            "word_count": count_words(document['content'])
+        }
+        summaries.append(summary)
+    return summaries
+
+
+
 print("Documents loaded successfully.")
 print_documents_id_and_title(documents)
 
@@ -43,4 +62,7 @@ second_document = get_document_by_id(documents, 2)
 if second_document:
     print(f"Second document content: {second_document['content']}")
 
-print_word_count_of_each_document(documents)
+summaries = summarize_documents(documents)
+print("Document summaries:")
+for summary in summaries:
+    print(f"Document ID: {summary['id']}, Title: {summary['title']}, Word Count: {summary['word_count']}")
